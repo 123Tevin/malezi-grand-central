063f38d072bfc143b80e2a86afbe2119b7a24e87
Terahpatrick <patrickmwangi554@gmail.com>
2021-05-11 07:52:42 -0400

    Changed socials colors

--- src/components/Shared/Socials/Socials.js
+++ src/components/Shared/Socials/Socials.js
@@ -7,40 +7,40 @@ export default function Socials({ twitter, fb, instagram, linkedin, youtube }) {
       <ul className="social__list">
         <li className="social__list--item">
           <a href={twitter} target="blank" className="social__list--link">
-            <svg className="social__list--icon fill--primary">
-              <use xlinkHref={`${sprite}#icon-twitter`} />
+            <svg className="social__list--icon">
+              <use xlinkHref={`${sprite}#icon-twitter`} fill="#077da2" />
             </svg>
             <span className="social__list--text">Twitter</span>
           </a>
         </li>
         <li className="social__list--item">
           <a href={fb} target="blank" className="social__list--link">
-            <svg className="social__list--icon fill--primary">
-              <use xlinkHref={`${sprite}#icon-facebook`} />
+            <svg className="social__list--icon ">
+              <use xlinkHref={`${sprite}#icon-facebook`} fill="#077da2" />
             </svg>
             <span className="social__list--text">Facebook</span>
           </a>
         </li>
         <li className="social__list--item">
           <a href={linkedin} target="blank" className="social__list--link">
-            <svg className="social__list--icon fill--primary">
-              <use xlinkHref={`${sprite}#icon-linkedin2`} />
+            <svg className="social__list--icon">
+              <use xlinkHref={`${sprite}#icon-linkedin2`} fill="#077da2" />
             </svg>
             <span className="social__list--text">LinkedIn</span>
           </a>
         </li>
         <li className="social__list--item">
           <a href={instagram} target="blank" className="social__list--link">
-            <svg className="social__list--icon fill--primary">
-              <use xlinkHref={`${sprite}#icon-instagram`} />
+            <svg className="social__list--icon">
+              <use xlinkHref={`${sprite}#icon-instagram`} fill="#077da2" />
             </svg>
             <span className="social__list--text">Instagram</span>
           </a>
         </li>
         <li className="social__list--item">
           <a href={youtube} target="blank" className="social__list--link">
-            <svg className="social__list--icon fill--primary">
-              <use xlinkHref={`${sprite}#icon-youtube`} />
+            <svg className="social__list--icon">
+              <use xlinkHref={`${sprite}#icon-youtube`} fill="#077da2" />
             </svg>
             <span className="social__list--text">YouTube</span>
           </a>
